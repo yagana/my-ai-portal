bd785d51f0a293f837ddcdd3cc7bddf491fe57d8
jungmin <you@gmail.com>
2026-02-26 16:43:13 +0900

    update

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello azure")
+from flask import Flask
+app = Flask(__name__)  # 이 'app' 변수 이름이 중요합니다!
+
+@app.route("/")
+def hello():
+    return "Hello! Azure Deployment Success!"
+
+if __name__ == "__main__":
+    app.run()
