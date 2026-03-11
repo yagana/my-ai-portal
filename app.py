cc8bad02d1639755894c07c7158d6aef934b4eae
jungmin <you@gmail.com>
2026-03-11 15:17:39 +0900

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)  # 이 'app' 변수 이름이 중요합니다!
 
 @app.route("/")
 def hello():
-    return "Hello! Azure Deployment Success!"
+    return "Hello! Azure Deployment Success!?????????"
 
 if __name__ == "__main__":
     app.run()
